d91a11337a93f4792ba6d041421479b03cf5ec28
serpiko <serpiko@hanmail.net>
2024-10-03 03:48:41 +0900

    FAQ 퍼블리싱 완료(반응형X)

---
+++ src/Pages/Staking/Components/FAQ/FaqBox.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { ReactComponent as ChevronRight } from '../../../../Assets/Icons/ChevronRight.svg';
+
+interface FaqBoxProps {
+  question: string;
+  answer: string;
+}
+
+export default function FaqBox(props: FaqBoxProps) {
+  const { question, answer } = props;
+  return (
+    <>
+      <div className={`question flex items-center justify-between text-[20px] pb-[12px] pl-[36px] `}>
+        <h3>{question}</h3>
+        <i className="icon pr-[36px]">
+          <ChevronRight />
+        </i>
+      </div>
+      <div
+        className="answer px-[31px] py-[21px] text-[#FFF] text-[16px] font-thin leading-[23px] mb-[14px]"
+        style={{ background: 'rgba(162, 132, 94, 0.80)' }}
+      >
+        {answer}
+      </div>
+    </>
+  );
+}
--- src/Pages/Staking/FrequentlyAskedQuestions.tsx
+++ src/Pages/Staking/FrequentlyAskedQuestions.tsx
@@ -1,28 +1,104 @@
 import PageLayout from '../../Components/PageLayout';
 import PageTitle from '../../Components/PageTitle';
+import { ReactComponent as InfoSign } from '../../Assets/Icons/InfoSign.svg';
+import { ReactComponent as FaqShapeLine } from '../../Assets/Images/FaqShapeLine.svg';
+
+import FaqBox from './Components/FAQ/FaqBox';
 
 const FrequentlyAskedQuestions: React.FC = () => {
   return (
     <PageLayout>
       {/* Grid */}
-      <div className="h-full grid grid-rows-1 grid-cols-2 gap-4 mt-[128px]">
+      <div className="h-full grid grid-rows-1 grid-cols-2 gap-[39px] mt-[128px]">
         {/* Header */}
         <div className="col-span-2 bg-blue-500 p-4 text-white text-center">
           <PageTitle title={'FAQ'} subTitle="We are the speakeasy for you to know too much" />
         </div>
 
-        <div>
-          <h2>Basic Info.</h2>
-          <div className="faq-container">
-            <div className="question">Q. What is $MOONDOG staking?</div>
-            <div className="answer">
-              Moondog staking allows users to lock their $MOONDOG tokens for a specified period in return for earning
-              rewards. This incentivizes long-term holding and contributes to the network’s stability and growth.
+        <div className="col-auto font-concert-one w-full max-w-[703px]">
+          <div className="title flex flex-col items-start">
+            <div className="flex items-center">
+              <i>
+                <InfoSign />
+              </i>
+              <h2 className="text-[32px] leading-none mx-[11px]">Basic Info.</h2>
+            </div>
+            <div className="my-[12px] w-full">
+              <FaqShapeLine />
             </div>
           </div>
+          <div className="faq-container py-[35px]" style={{ background: 'rgba(206, 166, 114, 0.49)' }}>
+            <FaqBox
+              question="Q. What is $MOONDOG staking?"
+              answer="Moondog staking allows users to lock their $MOONDOG tokens for a specified period
+in return for earning rewards. This incentivizes long-term holding and contributes 
+to the network’s stability and growth."
+            />
+            <FaqBox
+              question="Q. What is Moondog game?"
+              answer="The Moondog game is part of the play-to-earn ecosystem where users can engage in 
+              various fun and interactive activities. By playing, users can earn $MOONDOG tokens; 
+              moreover, the users in the top 100 will be rewarded with a bonus tokens. "
+            />
+            <FaqBox
+              question="Q. Wen?"
+              answer="The launch dates and specific features of Moondog staking and the games are 
+              announced via the official channels. Keep an eye on Moondog’s website and social 
+              media for updates."
+            />
+            <FaqBox
+              question="Q. How to stake $MOONDOG?"
+              answer="To stake $MOONDOG, users will connect their wallets to the Moondog platform, 
+              choose the amount to stake. After confirming the transaction, the tokens will be 
+              locked, and rewards will begin accruing."
+            />
+            <FaqBox
+              question="Q. When can I claim the rewards?"
+              answer="Rewards can typically be claimed at the end of the staking period which occurs weeklys"
+            />
+            <FaqBox
+              question="Q. Will there be benefits?"
+              answer="Yes, staking provides several benefits, including earning passive income through 
+              token rewards, gaining access to exclusive future drops, and possibly qualifying for
+               rare NFTs or additional bonuses."
+            />
+            <FaqBox
+              question="Q. Who can I contact for more info?"
+              answer="For more information, you can reach out via Moondog’s official social media channels, 
+              such as Telegram and Twitter."
+            />
+          </div>
         </div>
-        <div>
-          <h2>Game Play</h2>
+        <div className="col-auto font-concert-one w-full max-w-[703px]">
+          <div className="title flex flex-col items-start">
+            <div className="flex items-center">
+              <i>
+                <InfoSign />
+              </i>
+              <h2 className="text-[32px] leading-none mx-[11px]">Game Play</h2>
+            </div>
+            <div className="my-[12px] w-full">
+              <FaqShapeLine />
+            </div>
+          </div>
+          <div className="faq-container py-[35px]" style={{ background: 'rgba(206, 166, 114, 0.49)' }}>
+            <FaqBox question="Q. Is Moondog game free?" answer="Yes, the Moondog game is free to play." />
+            <FaqBox
+              question="Q. How does the game work?"
+              answer="The Moondog game operates on a play-to-earn model. Players can collect items 
+              in the game to score points and aim for the highest score."
+            />
+            <FaqBox
+              question="Q. Say more about the prize."
+              answer="Prizes in the Moondog game include $MOONDOG tokens, future drops that offer 
+              additional perks such as NFT, additional airdrops. Users in the top 100 can earn extra 
+              $MOONDOG."
+            />
+            <FaqBox
+              question="Q. How does the leaderboard work?"
+              answer="The Moondog leaderboard tracks players’ progress based on highest scores in the game."
+            />
+          </div>
         </div>
       </div>
     </PageLayout>
