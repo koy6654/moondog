90d8567fc961f4705b31e1225b2b6d47725869a9
serpiko <serpiko@hanmail.net>
2024-10-02 22:04:05 +0900

    faq-container 작성중...

--- src/Pages/Staking/FrequentlyAskedQuestions.tsx
+++ src/Pages/Staking/FrequentlyAskedQuestions.tsx
@@ -5,20 +5,25 @@ const FrequentlyAskedQuestions: React.FC = () => {
   return (
     <PageLayout>
       {/* Grid */}
-      <div className="h-full grid grid-rows-3 grid-cols-2 gap-4 mt-[128px]">
+      <div className="h-full grid grid-rows-1 grid-cols-2 gap-4 mt-[128px]">
         {/* Header */}
         <div className="col-span-2 bg-blue-500 p-4 text-white text-center">
           <PageTitle title={'FAQ'} subTitle="We are the speakeasy for you to know too much" />
         </div>
 
-        {/* Wallet */}
-        <div className="row-span-2 col-span-1 bg-green-500 p-4 text-white text-center">월렛</div>
-
-        {/* Game preview */}
-        <div className="col-span-1 bg-red-500 p-4 text-white text-center">게임</div>
-
-        {/* Leaderboard */}
-        <div className="col-span-1 bg-purple-500 p-4 text-white text-center">리더보드</div>
+        <div>
+          <h2>Basic Info.</h2>
+          <div className="faq-container">
+            <div className="question">Q. What is $MOONDOG staking?</div>
+            <div className="answer">
+              Moondog staking allows users to lock their $MOONDOG tokens for a specified period in return for earning
+              rewards. This incentivizes long-term holding and contributes to the network’s stability and growth.
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2>Game Play</h2>
+        </div>
       </div>
     </PageLayout>
   );
