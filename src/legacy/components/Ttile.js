42eaf22c86da8669236c4886e516990e8f936d78
koy6654 <koy410994@gmail.com>
2024-10-15 11:21:06 +0900

    legacy: 레거시 코드 제거

--- src/legacy/components/Ttile.js
+++
@@ -1,4 +0,0 @@
-"use client";
-export default function Ttile({ str }) {
-  return <div className="font-tusker text-5xl md:text-8xl mt-4 md:mt-10 mb-6 pb-6">{str}</div>;
-}
--- src/legacy/lib.js
+++
@@ -1,5 +0,0 @@
-export function shortenAddress(address, chars = 4) {
-  const start = address.slice(0, chars + 2); // 처음 2자리는 '0x'를 포함하므로 2를 더함
-  const end = address.slice(-chars);
-  return `${start}...${end}`;
-}
